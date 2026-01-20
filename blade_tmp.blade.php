b816f055bb0a25bb1f465cfcf0d0e065da9bb832
marceljogido <marcelmarbun33@gmail.com>
2026-01-20 17:21:42 +0700

    chore: cleanup project structure, move all langs to resources/lang

--- blade_tmp.blade.php
+++
@@ -1,5 +0,0 @@
-@if(session(''flash_success''))
-    <div class="mb-6 rounded-2xl border border-green-200 bg-green-50 p-4 text-green-800">
-        {{ session(''flash_success'') }}
-    </div>
-@endif
